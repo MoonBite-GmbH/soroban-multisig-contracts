2775bd04103991e19c74181a788a3ae81a55fe89
Kaloyan Gangov <6922910+gangov@users.noreply.github.com>
2024-08-26 12:52:05 +0300

    create update prosal with timestamp

--- contracts/multisig/src/tests/update_proposal.rs
+++ contracts/multisig/src/tests/update_proposal.rs
@@ -1,4 +1,9 @@
-use soroban_sdk::{testutils::Address as _, vec, Address, Env, String};
+use soroban_sdk::{
+    testutils::{Address as _, Ledger},
+    vec, Address, Env, String,
+};
+
+use crate::tests::setup::{DAY_AS_TIMESTAMP, TWO_WEEKS_EXPIRATION_DATE};
 
 use super::setup::initialize_multisig_contract;
 
@@ -106,4 +111,51 @@ fn create_update_proposal_should_fail_when_invalid_expiration_date() {
     let new_wasm_hash = utils::multisig_wasm_hash(&env);
     // 1 second less than an hour
     multisig.create_update_proposal(&member1, &new_wasm_hash, &Some(3_599));
+}
+#[test]
+fn create_and_execute_update_proposal_with_expiration_date() {
+    let env = Env::default();
+    env.mock_all_auths();
+    env.budget().reset_unlimited();
+
+    let member1 = Address::generate(&env);
+    let member2 = Address::generate(&env);
+    let member3 = Address::generate(&env);
+    let members = vec![&env, member1.clone(), member2.clone(), member3.clone()];
+
+    let multisig = initialize_multisig_contract(
+        &env,
+        String::from_str(&env, "MultisigName"),
+        String::from_str(&env, "Example description of this multisig"),
+        members.clone(),
+        Some(5_000u32),
+    );
+
+    // it's not actually a new wasm hash, but we need to use it to test the update proposal
+    let new_wasm_hash = utils::multisig_wasm_hash(&env);
+    multisig.create_update_proposal(&member1, &new_wasm_hash, &Some(TWO_WEEKS_EXPIRATION_DATE));
+
+    let proposal_id = multisig.query_last_proposal_id();
+
+    env.ledger().with_mut(|li| li.timestamp = DAY_AS_TIMESTAMP);
+    multisig.sign_proposal(&member1, &proposal_id);
+
+    env.ledger()
+        .with_mut(|li| li.timestamp = DAY_AS_TIMESTAMP * 2);
+    multisig.sign_proposal(&member2, &proposal_id);
+
+    env.ledger()
+        .with_mut(|li| li.timestamp = DAY_AS_TIMESTAMP * 3);
+    multisig.sign_proposal(&member3, &proposal_id);
+
+    let version_proposal = multisig.query_multisig_info().version_proposal;
+    assert_eq!(version_proposal, 0);
+
+    // the day before the endtime we sign the update proposal
+    env.ledger()
+        .with_mut(|li| li.timestamp = TWO_WEEKS_EXPIRATION_DATE - DAY_AS_TIMESTAMP);
+    multisig.execute_proposal(&member1, &proposal_id);
+
+    let version_proposal = multisig.query_multisig_info().version_proposal;
+    assert_eq!(version_proposal, 1);
 }
